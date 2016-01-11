9f669508897d0f14bd9f22fa7184442281c6bd05
emerj4181 <emerj4181@GCI-IC0026970.wrdsb.ca>
2016-01-11 10:05:42 -0500

    :3

---
+++ src/student/report/card/oop/AverageTest.java
@@ -0,0 +1,15 @@
+
+package student.report.card.oop;
+
+/**
+ *
+ * @author emerj4181
+ */
+public class AverageTest {
+
+    public static void main(String[] args) {
+        Student Joel = new Student();
+        
+
+    }
+}
--- src/student/report/card/oop/Student.java
+++ src/student/report/card/oop/Student.java
@@ -1,9 +1,33 @@
-
 package student.report.card.oop;
 
 public class Student {
-    int markCourse[][]=new int[4][2];
+
+    String markCourse[][] = new String[4][2];
     int studentNumber;
+    int average;
     String lastName;
     String firstName;
+
+    public int calulateAverage() {
+        int markSum = 0;
+        for (int j = 0; j < 4; j++) {
+            int markTemp = Integer.parseInt(markCourse[j][1]);
+            markSum = markSum + markTemp;
+        }
+        average = markSum / 4;
+        return average;
+    }
+    public String lastName(String lName){
+        lastName=lName;
+        return lastName;
+    }
+    public String firstName(String fName){
+        firstName=fName;
+        return firstName;
+    }
+    public int studentNumber(int sNumber){
+        studentNumber=sNumber;
+        return studentNumber;
+    }
+    
 }
